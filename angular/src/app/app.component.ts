16d7539349e1e67f836235018b9fad61ef0d17cd
meir juli <mybs2323@gmail.com>
2024-01-08 21:32:42 +0200

    Angular imp fixed

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import Main from '../../../../main'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'dynamic-pagination'
+
+  mainInc: Main = new Main()
+  displayList: any[] = []
+  end: boolean = false
+
+  async ngOnInit(): Promise<void> {
+    await this.mainInc.init()
+
+    this.displayList = this.mainInc.getDisplayList()
+  }
+
+  async fetchMoreItems(): Promise<void> {
+    const newSlice = await this.mainInc.paginate()
+
+    if (newSlice.length === this.displayList.length)
+      this.end = true
+
+    else this.displayList = newSlice
+  }
+}
